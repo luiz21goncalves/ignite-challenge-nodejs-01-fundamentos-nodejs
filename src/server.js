e5bbfd51b66586828457ab0c60694817b92068ee
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2023-02-10 20:09:49 -0300

    chore: add json middleware

--- src/server.js
+++ src/server.js
@@ -1,11 +1,15 @@
 import dotenv from 'dotenv'
 import http from 'node:http'
+
+import { json } from './middlewares/json.js'
 import { routes } from './routes.js'
 
 dotenv.config()
 
-const server = http.createServer((request, response) => {
+const server = http.createServer(async (request, response) => {
   const { method, url } = request
+
+  await json(request, response)
 
   const route = routes.find((route) => {
     return route.method === method && route.path === url
